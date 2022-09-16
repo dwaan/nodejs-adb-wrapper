510e3f1edb9ca2da8253bf015dde9bf46bd3bd1a
Dwan <me@dwaan.com>
2022-09-16 17:05:39 +0300

    FIX: wrongly report device is unautorized when there are more than 1 device

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ class adb extends EventEmitter {
         device.message.split("\n").forEach(output => {
             if (output.includes(`${this.ip}`)) message = output;
         });
-        message = device.message.toLowerCase();
+
         if (connect.message.includes(`device still authorizing`)) result = this.DEVICE_AUTHORIZING;
         else if (message.includes(`unauthorized`)) result = this.DEVICE_UNAUTHORIZED;
         else if (connect.message.includes(`operation timed out`) || connect.message.includes(`timeout`)) result = this.TIME_OUT;
